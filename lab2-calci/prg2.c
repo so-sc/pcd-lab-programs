2eb95310290eae3ea1e02b5ec549992efb5e84d6
abdulraouf9567 <raouf9567@yahoo.com>
2018-10-27 13:37:17 -0700

    A slight change in program 2 eg:+,-etc

--- lab2-calci/prg2.c
+++ lab2-calci/prg2.c
@@ -2,24 +2,33 @@
 #include<stdlib.h>
 void main()
 {
-float a,b,c;
-char o;
-printf("enter a,o,b \n");
-scanf("%f%s%f",&a,&o,&b);
-switch(0)
+int a,b,z;
+char op;
+printf("enter a,b \n");
+scanf("%d%c%d",&a,&b);
+printf("\n_______MENU_______");
+printf("\n\t\t1.ADDITION "+"");
+printf("\n\t\t2.SUBTRACTION "-"");
+printf("\n\t\t3.MULTIPLICATION "*"");
+printf("\n\t\t4.DIVISION "/"");
+printf("Enter the choice")
+scanf("%c",&op);
+switch(op)
 {
 case '+':z=a+b;
-          printf("res=%f",z);
+          printf("res=%d",z);
              break;
 case '-':z=a-b;
-          printf("res=%f",z);
+          printf("res=%d",z);
            break;
 case '*':z=a*b;
-         printf("res=%f",z);
+         printf("res=%d",z);
 break;
 case '/':z=a/b;
-         printf("res=%f",z);
+         printf("res=%d",z);
 break;
-default :printf("invalid option ");
-}
 
+default :printf("invalid option ");
+break;
+}
+}
